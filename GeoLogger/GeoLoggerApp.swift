179bb685be1db46606c9e5d54f27b0da1c6bf5e6
maciekp9 <maciek.mp15@gmail.com>
2022-04-20 14:40:20 +0200

    Initial Commit

---
+++ GeoLogger/GeoLoggerApp.swift
@@ -0,0 +1,20 @@
+//
+//  GeoLoggerApp.swift
+//  GeoLogger
+//
+//  Created by Maciej Przydatek on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoLoggerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
